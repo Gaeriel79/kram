7c6b832c256c2e7e1b163b6406b63fa97adfb215
Gaeriel79 <92832022+Gaeriel79@users.noreply.github.com>
2021-11-05 04:36:15 +0100

    Create rpiinit.sh

---
+++ rpiinit/rpiinit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Systemupdates
+apt get update && apt full-upgrade
+
+#Netzwerkkonfiguration
+#hostnamen setzen
+nano /etc/dhcpcd.conf
+hostnamectl set-hostname dockerpi
+nano /etc/hosts
+
+#Zeitzone
+timedatectl set-timezone Europe/Berlin
+
+
+#SSH Konfiguration
+nano /etc/ssh/sshd_config
+mkdir ~/.ssh && chmod 700 ~/.ssh
+
+
+# Programme
+apt install htop neofetch vim docker-ce
+
+#Raspi-Config
+#locale keyboard expand filesystem
+raspi-config
+
+#Benutzer ändern
+usermod -l pi gaeriel
+usermod -d /home/gaeriel -m gaeriel
+groupmod -n gaeriel pi
+passwd gaeriel
+
+#portainer Installation
+docker volume create portainer_data && sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
