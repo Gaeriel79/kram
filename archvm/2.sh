8d333cad8a2942786083f936b0affac704e1a40b
Gaeriel79 <92832022+Gaeriel79@users.noreply.github.com>
2022-01-11 08:01:39 +0100

    #

---
+++ archvm/2.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+#Dateisystem erstellen
+mkfs.btrfs /dev/sda1
+
+#Mount root Partition
+mount /dev/sda1 -mnt
+
+#Subvolumes erstellen
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var
+
+#Unmount root Partition
+umount /mnt
+
+#Mounten der Subvolumes
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2, subvol=@ /dev/sda1 /mnt
+mkdir -p /mnt/{home,var}
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2, subvol=@home /dev/sda1 /mnt/home
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2, subvol=@var /dev/sda1 /mnt/var
+
+#Installieren der Basis Softwarepakete
+pacstrap /mnt base linux-zen linux-firmware git vim nano btrfs-progs tldr open-vm-tools xf86-input-vmmouse xf86-video-vmware timeshift htop neofetch grub networkmanager network-manager-applet mtools dosfstools base-devel linux-zen-headers xdg-utils xdg-user-dirs
+
+#Dateisystem erstellen
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+cat /etc/fstab
+
+#Zeitzone und locale einstellen
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+sed -i 's/#de_DE.UTF8 UTF-8/de_DE.UTF8 UTF-8/' /etc/locale-gen
+locale-gen
+echo "LANG=de_DE.UTF8" >> /etc/locale.config
+
+#Tastatur einstellen
+echo "KEYMAP=de-latin1" >> /etc/vconsole.config
+
+#Hostnamen festlegen
+echo "archvm" >> /etc/hostname
+
+#hosts Datei anpassen
+echo "127.0.0.1     localhost" | tee -a /etc/hosts
+echo "::1           localhost" | tee -a /etc/hosts
+echo "127.0.1.1     archvm.localdomain      archvm" | tee -a /etc/hosts
+
+#Rootpasswort setzen
+passwd
+
+#Grub einrichten
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
