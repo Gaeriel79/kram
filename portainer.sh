9276b2cfce83464be731efd5b3d37d34802aacde
Gaeriel79 <92832022+Gaeriel79@users.noreply.github.com>
2022-01-12 01:12:18 +0100

    Anleitung ergänzt

---
+++ portainer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#Anleitung: 
+#
+#Starten mit sudo ./portainer.sh
+
+#Laufenden Container stoppen und entfernen
+docker stop portainer && sudo docker rm portainer
+
+#Neues Image herunterladen
+docker pull portainer/portainer-ce:latest
+
+#Neuen Container starten
+docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
