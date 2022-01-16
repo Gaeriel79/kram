92428c222330c88893a0f1554b1aad2beb2b9bed
Gaeriel79 <92832022+Gaeriel79@users.noreply.github.com>
2022-01-16 20:50:31 +0100

    Add files via upload

---
+++ pacstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+pacstrap /mnt base linux-zen linux-firmware git vim nano btrfs-progs tldr kitty htop neofetch grub networkmanager network-manager-applet mtools dosfstools base-devel linux-zen-headers xdg-utils xdg-user-dirs wget reflector rsync grub os-prober dhclient intel-ucode libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa bluez bluez-libs bluez-utils cronie cups efibootmgr ranger picom snapper sudo usbutils --noconfirm --needed
