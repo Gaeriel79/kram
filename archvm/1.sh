02f16f92786ba1468fc3e0f539e72c85e1e0b4ad
Gaeriel79 <92832022+Gaeriel79@users.noreply.github.com>
2022-01-22 07:36:27 +0100

    #

--- archvm/1.sh
+++ archvm/1.sh
@@ -7,10 +7,14 @@ setfont /usr/share/kbd/consolefonts/ter-132n.psf.gz
 #Keyboard Layout
 loadkeys de-latin1
 
+wifi-menu
 #Time
 timedatectl set-ntp true
 
 #pacman sync check
-pacman -Sy
+pacman -Syyy
+pacman -S reflector
+reflector -c Germany -a 6 --sort --save /etc/pacman.de/mirrorlist
+pacman -Syyy
 
 chmod +x k2.sh
---
+++ archvm/readme.md
@@ -0,0 +1,9 @@
+Befehle zur Einrichten des Laufwerks mit BTRFS und ZRAM mit CSM
+lsblk
+
+gdisk /dev/sda
+
+n
+1
+8300
+w
